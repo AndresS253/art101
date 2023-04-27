10934a60eb995be9575965e6aa35de2907862938
AndresS253 <130080033+AndresS253@users.noreply.github.com>
2023-04-27 08:21:43 -0700

    minor changes

--- lab5/js/lab.js
+++ lab5/js/lab.js
@@ -1,6 +1,6 @@
 // index.js - purpose and description here
-// Author: Your Name
-// Date:
+// Author: Andres Sanchez
+// Date: 4/26/23
 
 // Constants
 
@@ -13,8 +13,22 @@ function myFunction(param1, param2) {
 }
 
 function main() {
+  
   console.log("Main function started.");
   // the code that makes everything happen
+  var make='BMW';
+  var model='128i';
+  var color='White';
+  var year=2008;
+
+  var age=2023-year;
+
+  document.writeln('Make: ' + make +'<br>');
+  document.writeln('Model: ' + model +'<br>');
+  document.writeln('Color: ' + color +'<br>');
+  document.writeln('Year: ' + year +'<br>');
+  document.writeln('Age: ' + age +'years<br>');
+  
 }
 
 // let's get this party started
