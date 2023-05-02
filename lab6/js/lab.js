77e1637cc6ba1a4b50986d5d954165ae29b19575
AndresS253 <130080033+AndresS253@users.noreply.github.com>
2023-05-02 01:15:35 -0700

    lab 6

--- lab6/js/lab.js
+++ lab6/js/lab.js
@@ -3,18 +3,27 @@
 // Date:
 
 // Constants
+var YEAR=2023;
 
 // Functions
 
-// this is an example function and this comment tells what it doees and what parameters are passed to it.
-function myFunction(param1, param2) {
-  // some code here
-  // return results;
-}
 
+var myMainRide = {
+  make: 'BMW',
+  model: '128i',
+  color: 'White',
+  year: 2008,
+  age: function() {return 2023-this.year;}
+}
+var myTransport=['cheverlegs','car','bicycle'];
 function main() {
   console.log("Main function started.");
+  
   // the code that makes everything happen
+  document.writeln("Getting around: " + myTransport + "<br>");
+  document.writeln("My Main Ride: <pre>", 
+  JSON.stringify(myMainRide, null, '\t'), "</pre>");
+
 }
 
 // let's get this party started
