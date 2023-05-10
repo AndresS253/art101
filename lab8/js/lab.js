474b1bfd2b23f5048fb5a125a5d4a31df422c852
AndresS253 <130080033+AndresS253@users.noreply.github.com>
2023-05-09 20:04:44 -0700

    lab 8

--- lab8/js/lab.js
+++ lab8/js/lab.js
@@ -1,21 +1,29 @@
-// index.js - purpose and description here
-// Author: Your Name
-// Date:
+// index.js - anonymous functions and callbacks
+// Author: Andres Sanchez
+// Date:5/9/23
 
-// Constants
 
-// Functions
-
-// this is an example function and this comment tells what it doees and what parameters are passed to it.
-function myFunction(param1, param2) {
-  // some code here
-  // return results;
-}
 
 function main() {
   console.log("Main function started.");
-  // the code that makes everything happen
+  function addOne(x){//anonymous function that adds one
+    var results = x+1
+    return results;
+}
+console.log("1+1 = ",addOne(1));//testing function
+console.log("1+2 = ",addOne(2));
+
+  var someArray = [10,100,200,300]; //an array
+  console.log("My starting array",someArray); //printing the array
+  
+  
+  console.log("Add one to all numbers in an array", someArray.map(addOne));//printed results of the anonymous
+                                     //function acting on the array
+  
+  console.log("Multiplies all numbers in an array by 2",someArray.map(function(x){//printed result of callback function
+  return x*2;                           //on an array
+}))
+  
 }
 
-// let's get this party started
 main();
