28b637e994eb9ad2a95376e0f6bacc5aad6c4bdf
AndresS253 <130080033+AndresS253@users.noreply.github.com>
2023-05-11 00:26:15 -0700

    made changes to lab 8 for task x, and completed lab9
    made changes to lab 8 for task x, and completed lab9

--- lab8/js/lab.js
+++ lab8/js/lab.js
@@ -6,15 +6,39 @@
 
 function main() {
   console.log("Main function started.");
+
   function addOne(x){//anonymous function that adds one
     var results = x+1
     return results;
 }
+  var someArray = [10,100,200,300]; //an array
+  console.log("My starting array",someArray); //printing the array
+
+  var outputEl = document.getElementById("output"); // gets the output
+  var new1El = document.createElement("p");//Creating string elements to hold output
+  var new2El = document.createElement("p");
+  var new3El = document.createElement("p");
+
+  new1El.innerHTML = "My starting array ["+ someArray+']';//initial array
+  new1El.id = "array";
+
+  new2El.innerHTML = "Add one to all numbers in an array [" + someArray.map(addOne)+']';//initial array el +1
+  new2El.id = "array2";
+  
+  new3El.innerHTML ="Multiplies all numbers in an array by 2 ["+someArray.map(function(x){
+    return x*2;                           
+  })+']';
+  new3El.id = "array3";
+
+  outputEl.appendChild(new1El);//appends elements to the id
+  outputEl.appendChild(new2El);
+  outputEl.appendChild(new3El);
+
+  
 console.log("1+1 = ",addOne(1));//testing function
 console.log("1+2 = ",addOne(2));
 
-  var someArray = [10,100,200,300]; //an array
-  console.log("My starting array",someArray); //printing the array
+  
   
   
   console.log("Add one to all numbers in an array", someArray.map(addOne));//printed results of the anonymous
@@ -22,6 +46,8 @@ console.log("1+2 = ",addOne(2));
   
   console.log("Multiplies all numbers in an array by 2",someArray.map(function(x){//printed result of callback function
   return x*2;                           //on an array
+
+  
 }))
   
 }
--- lab9/js/lab.js
+++ lab9/js/lab.js
@@ -15,6 +15,28 @@ function myFunction(param1, param2) {
 function main() {
   console.log("Main function started.");
   // the code that makes everything happen
+  var outputEl = document.getElementById("output");
+  console.log("outputEl: ", outputEl);
+
+  var new1El = document.createElement("p");
+
+  new1El.innerHTML = "Pre-evolution";
+  new1El.id = "new-one";
+  new1El.style.color="red"
+
+  var new2El = document.createElement("p");
+  new1El.id = "new-two";
+
+  new2El.innerHTML = "It evolved!";
+  new2El.style.color="blue"
+
+  outputEl.appendChild(new1El);
+  outputEl.appendChild(new2El);
+
+  outputEl.style.border = "dashed 5px grey";
+  outputEl.className = "changed";
+
+  //console.log(outputEl);
 }
 
 // let's get this party started
